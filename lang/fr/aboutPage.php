6cb57af92e21ade112ebbb38ac1258a129f8ac95
thehocinesaad <saadhocine0@gmail.com>
2022-06-29 20:00:10 +0100

    finished about page

--- lang/fr/aboutPage.php
+++ lang/fr/aboutPage.php
@@ -12,4 +12,10 @@ return [
     'tabTitle' => ':websiteTitle | Qui nous sommes',
     'title'   => 'Notre mission',
     'description' => "Sauver des vies en Algérie en rendant la recherche de donneurs de sang aussi simple qu'une recherche sur Google.",
+    'whoAreWe' => "Qui sommes-nous",
+    'whoAreWeDescription' => "Un jeune algérien de la wilaya de Tizi Ouzou qui cherche à faire une chose de simple et beau pour son pays et pour le peuple algerien en aidant les personnes qui ont besoin de sang à trouver des donneurs de sang rapidement, facilement et dans leur région.",
+    "motivation" => "Notre motivation",
+    "motivationDescription" => "Combien de fois quelqu'un vous a-t-il appelé pour vous dire qu'il avait besoin de vous pour donner du sang ? Ou combien de publications avez-vous rencontrées sur Facebook où ils demande des donneurs de sang ? Je suis sûr que cela vous est arrivé plusieurs fois, tout comme cela m'est arrivé personnellement, au point que l'on peut dire que trouver des donneurs de sang est devenu difficile et un problème pour beaucoup de gens, et parfois cela peut être un problème de vie ou de mort , cette chose m'a toujours motivé à trouver une solution et j'espère que cette plateforme sera la solution.",
+    'vision' => "Notre vision",
+    'visionDescription' => "C'est simple, trouver du sang ne sera plus un problème en Algerie, vous avez besoin de donneurs de sang ? une recherche rapide sur cette platform vous fournira des milliers de donneur près de chez vous.",
 ];
