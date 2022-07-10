76bde8fc1c8308503f5ccd15aa048b5c9d720173
Zaoui Mohammed Amine <76101501+ZaouiAmine@users.noreply.github.com>
2022-07-10 20:20:34 +0100

    about page English version 1.0.0
    translated the about page to English

---
+++ lang/en/aboutPage.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| About Page Language Lines
+|--------------------------------------------------------------------------
+|
+|
+*/
+
+return [
+    'tabTitle' => ':websiteTitle | Who are we ',
+    'title'   => 'Our Mission', 
+    'description' => "Saving lives in Algeria through making the search for donors as easy as searcing Google.",
+    'whoAreWe' => "Who are we",
+    "whoAreWeDescription" => "An Algerian from Tizi Ouzou that strives giving back to his country and his people through helping the search for blood donors.",
+    'motivation' => "The motive",
+    "motivationDescription" => 'How many times have you been asked to donate ? how many posts have you encountered on social media asking for blood donors ? many times without a single doubt just like I experienced , the search for donations and donors became something very hard , and since it is a life and death situation I took it upon myself to find a solution and to develop this website .',
+    'vision' => "Our vision",
+    'visionDescription' => "Simple , finding blood in Algeria never been easier . In a need of donors ? the search functionality is so fast it lets you reach thousands of them near you .",
+];
