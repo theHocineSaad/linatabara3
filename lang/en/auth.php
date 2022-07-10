b8d0b22b6c251188aeda05f6dfb1c857ece5a649
Zaoui Mohammed Amine <76101501+ZaouiAmine@users.noreply.github.com>
2022-07-10 20:38:18 +0100

    added the English version of the auth.php
    translated auth.php

---
+++ lang/en/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Entered information does not match our records .',
+    'password' => 'The entered password is incorrect .',
+    'throttle' => 'The number of attempts has been exceeded . please try again in :seconds seconds',
+
+];
