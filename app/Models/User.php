c7cb64b950b23e80f46fdfe48dfacd145f5adc08
thehocinesaad <saadhocine0@gmail.com>
2022-07-15 15:58:32 +0100

    fixed the name of a Model

--- app/Models/User.php
+++ app/Models/User.php
@@ -32,7 +32,7 @@ class User extends Authenticatable
 
     public function bloodGroup()
     {
-        return $this->belongsTo(Blood::class);
+        return $this->belongsTo(BloodGroup::class);
     }
 
     public function wilaya()
