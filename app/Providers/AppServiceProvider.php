6411df0175f2875dca55ef4d75ed93798ab46f32
thehocinesaad <saadhocine0@gmail.com>
2022-07-23 10:53:36 +0100

    added UserObserver to AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\User;
+use App\Observers\UserObserver;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 
@@ -14,5 +16,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::useBootstrapFive();
+
+        User::observe(UserObserver::class);
     }
 }
